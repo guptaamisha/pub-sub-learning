01d918df33c5a24e4e0159c8d5c1ebf3ef11a677
amisha-sendinblue <amishagupta1412@gmail.com>
2022-07-17 14:26:52 +0530

    initial commit

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println("ID: ", id)
+}
